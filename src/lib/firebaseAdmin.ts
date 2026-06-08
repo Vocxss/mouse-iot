c3758179f0d89fc0d2646b29e2647e413051f8fc
Vocxss <tsqf.h29@gmail.com>
2026-06-08 10:01:27 +0700

    refactor: Change service account at firebase configuration

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,12 +1,25 @@
-import admin from "firebase-admin"
+import admin from "firebase-admin";
 
-import serviceAccount from "./adalah-pokoknya-56795-firebase-adminsdk-fbsvc-369fd10cf8.json";
+const serviceAccount = {
+  type: process.env.NEXT_PUBLIC_TYPE,
+  project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
+  private_key_id: process.env.NEXT_PUBLIC_PRIVATE_KEY_ID,
+  private_key: process.env.NEXT_PUBLIC_PRIVATE_KEY,
+  client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+  client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+  auth_uri: process.env.NEXT_PUBLIC_AUTH_URI,
+  token_uri: process.env.NEXT_PUBLIC_TOKEN_URI,
+  auth_provider_x509_cert_url:
+    process.env.NEXT_PUBLIC_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.NEXT_PUBLIC_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.NEXT_PUBLIC_UNIVERSE_DOMAIN,
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-    databaseURL: "https://adalah-pokoknya-56795-default-rtdb.asia-southeast1.firebasedatabase.app"
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   });
 }
 
-export const authAdmin = admin.auth()
+export const authAdmin = admin.auth();
